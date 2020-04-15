fe68d9841f954aaed83225179a04eabfc42f55af
Feinasia <feinasia@gmail.com>
2020-04-15 21:30:17 +0800

    YZU Python

--- lesson02/Case08.py
+++ lesson02/Case08.py
@@ -1,5 +1,9 @@
 # -*- coding:UTF-8 -*-
 
+import math
 n = int(input('請輸入數字'))
 odd = n % 2 == 00
 print("%d 是偶數嗎? %s" % (n, odd))
+
+odd2 = math.fmod(n,2)==0
+print("%d 是偶數嗎? %s" % (n, odd2))
--- lesson02/Case09.py
+++ lesson02/Case09.py
@@ -1,4 +1,5 @@
 # -*- coding:UTF-8 -*-
+import math
 
 #BMI 資料運算
 h = float(input('請輸入身高(cm):'))
@@ -6,3 +7,7 @@ w = float(input('請輸入體重(kg):'))
 bmi = w / ((h/100)**2)
 result = 18<= bmi < 24
 print('身高: %.1f cm; 體重: %.1f Kg; BMI: %.2f; Result: %s' % (h, w, bmi, result))
+
+bmi2 = w / math.pow(h/100,2)
+result = 18<= bmi2 < 24
+print('身高: %.1f cm; 體重: %.1f Kg; BMI: %.2f; Result: %s' % (h, w, bmi2, result))
