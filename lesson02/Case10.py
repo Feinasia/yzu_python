ad7cb9f6a46f956a7aa8d2c23973415dc2ec4025
Feinasia <feinasia@gmail.com>
2020-04-15 21:32:39 +0800

    YZU Python

--- lesson02/Case10.py
+++ lesson02/Case10.py
@@ -1,7 +1,10 @@
 # -*- coding:UTF-8 -*-
+import math
 
 x1, y1 = 10, 20
 x2, y2 = 30, 40
 # 求2點間的距離 d
 d= ((x1-x2)**2 + (y1-y2)**2)**0.5
+d1=math.sqrt(math.pow(x1-x2, 2)+math.pow(y1-y2, 2))
 print(d)
+print(d1)
