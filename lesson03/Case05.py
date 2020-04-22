1901652a56f4d8ec2b260c429385a82e6025665d
Feinasia <feinasia@gmail.com>
2020-04-22 19:44:07 +0800

    YZU Python

---
+++ lesson03/Case05.py
@@ -0,0 +1,8 @@
+def mask(money):
+    x = money // 5  # "//" 指整除
+    size = "Audlts"
+    return x, size
+
+
+count, Mysize = mask(100)
+print(count, Mysize)
--- lesson03/Casee05.py
+++
@@ -1,7 +0,0 @@
-def mask(money):
-     x = money // 5  # "//" 指整除
-     size = "Audlts"
-     return x, size
-
-count, Mysize = mask(100)
-print(count, Mysize)
