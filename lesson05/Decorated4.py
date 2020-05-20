757ac8f7abded1bff33dc8b8f5355b8385318563
Feinasia <feinasia@gmail.com>
2020-05-20 18:43:37 +0800

    YZU Python

--- lesson05/Decorated4.py
+++ lesson05/Decorated4.py
@@ -1,14 +1,16 @@
 ##未完成
 
-def login(func, password):
-    def check():
-        if password == 1234:
-            print('Succeed')
-            func()
-        else:
-            print('Failed')
-            return None
-    return check
+def login(password):
+    def decorated(func):
+        def check():
+            if password == 1234:
+                print('Succeed')
+                func()
+            else:
+                print('Failed')
+                return None
+        return check
+    return decorated
 
 @login(password=1234)
 def report():
